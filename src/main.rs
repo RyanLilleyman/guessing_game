86400684c72115dd2cd2c55e66ce2849bb73e100
RyanLilleyman <lilleymanryan2020@gmail.com>
2023-08-14 18:37:12 -0400

    Add back the base of the game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    loop {
+        println!("Please guess the number!");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Equal => {
+                println!("You win!");
+                println!(
+                    "You guessed: {}, the secret number was: {}",
+                    guess, secret_number
+                );
+                break;
+            }
+            Ordering::Greater => {
+                println!("Too High!");
+                continue;
+            }
+            Ordering::Less => {
+                println!("Too Low!");
+                continue;
+            }
+        }
+    }
 }
